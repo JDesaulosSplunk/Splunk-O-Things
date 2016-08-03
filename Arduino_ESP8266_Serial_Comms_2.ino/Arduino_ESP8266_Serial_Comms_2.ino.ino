9d1a88a0213e589d9212b36c6133952b36c39235
JDesaulosSplunk <jdesaulos@splunk.com>
2016-08-03 09:51:23 -0500

    Arduino Pro Mini Test Code
    Test code for the Arduino Pro Mini for Serial Communications with an
    ESP8266.

--- Arduino_ESP8266_Serial_Comms_2.ino/Arduino_ESP8266_Serial_Comms_2.ino.ino
+++ Arduino_ESP8266_Serial_Comms_2.ino/Arduino_ESP8266_Serial_Comms_2.ino.ino
@@ -20,8 +20,36 @@ DHT dht(DHTPIN, DHTTYPE);
 SoftwareSerial ESPserial(2, 3); // RX | TX
 float f;
 char buffer[6];
-
+char inData[20]; // Allocate some space for the string
+char inChar=-1; // Where to store the character read
+byte index = 0; // Index into array; where to store the character
  
+//http://stackoverflow.com/questions/5697047/convert-serial-read-into-a-useable-string-using-arduino
+char Comp(char* This) {
+    while (Serial.available() > 0) // Don't read unless
+                                   // there you know there is data
+    {
+        if(index < 19) // One less than the size of the array
+        {
+            inChar = Serial.read(); // Read a character
+            inData[index] = inChar; // Store it
+            index++; // Increment where to write next
+            inData[index] = '\0'; // Null terminate the string
+        }
+    }
+
+    if (strcmp(inData,This)  == 0) {
+        for (int i=0;i<19;i++) {
+            inData[i]=0;
+        }
+        index=0;
+        return(0);
+    }
+    else {
+        return(1);
+    }
+}
+
 void setup() 
 {
     Serial.begin(9600);     // communication with the host computer
@@ -40,15 +68,18 @@ void loop()
 {
     f = dht.readTemperature(true);
     String tf = dtostrf(f, 4, 1, buffer);
+  //  Serial.println(tf);
   
     // listen for communication from the ESP8266 and then write it to the serial monitor
     if ( ESPserial.available() )   {  Serial.write( ESPserial.read() );  }
  
     // listen for user input and send it to the ESP8266
-    if ( Serial.available() )
+    if ( Comp("1: ") == 0)
       {
+        Serial.println("found");
         //ESPserial.print(tf); 
-        ESPserial.write(tf);  
+        ESPserial.print(tf);  
         //delay(15000);
       }
 }
+
