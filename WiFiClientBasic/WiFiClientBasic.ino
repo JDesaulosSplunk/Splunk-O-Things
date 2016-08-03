9c211e8e52253959d74f1156aecef86ea08c0620
JDesaulosSplunk <jdesaulos@splunk.com>
2016-08-03 09:55:22 -0500

    Modification for Adafruit Feather Huzzah
    Code to send temperature data to Splunk from an Adafruit Feather
    Huzzah.

--- WiFiClientBasic/WiFiClientBasic.ino
+++ WiFiClientBasic/WiFiClientBasic.ino
@@ -5,6 +5,12 @@
 
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
+#include "DHT.h"
+#define DHTPIN 2 
+#define DHTTYPE DHT11   // DHT 11
+DHT dht(DHTPIN, DHTTYPE); 
+float f;
+char buffer[6];
 
 ESP8266WiFiMulti WiFiMulti;
 
@@ -33,13 +39,14 @@ void setup() {
 }
 
 
+
 void loop() {
     const uint16_t port = 2319;
     const char * host = "192.168.10.82"; // ip or dns
-    // char tf = Serial.read();
-    String tf = Serial.readString();
-    
-    
+
+    f = dht.readTemperature(true);
+    String tf = dtostrf(f, 4, 1, buffer);
+    Serial.println(tf);
     Serial.print("connecting to ");
     Serial.println(host);
 
@@ -54,13 +61,15 @@ void loop() {
     }
 
     // This will send the request to the server
+    //Serial.print("1: " + tf);
+    delay(5000);
     client.print("Send this data to server");
     client.print(tf);
-    Serial.print("1: %s", tf);
+
 
     //read back one line from server
-    String line = client.readStringUntil('\r');
-    client.println(line);
+    //String line = client.readStringUntil('\r');
+    //client.println(line);
 
     Serial.println("closing connection");
     client.stop();
